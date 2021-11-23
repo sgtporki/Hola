26575f30c41c970754d93fe9863906b1a06ecf52
edgar lopez <toro_edgar@hotmail.com>
2021-11-23 00:08:13 -0800

    hola

---
+++ Hola.js
@@ -0,0 +1,13 @@
+'use strict'
+
+console.log('Hola mundo, se tiene que ver en consola')
+
+console.log(2*5)
+
+
+console.log(global)
+
+
+setInterval(function(){
+    console.log('hola modejs');
+},1000)
